35658d701253e0bd5bbb14015c2ad02ce38efdd2
lea927 <lxthym@icloud.com>
2021-08-08 00:44:08 +0800

    Fixes merge conflicts on category model and index

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,4 @@
 class Category < ApplicationRecord
     has_many :tasks, dependent: :destroy # this mean if category is destroy task will destroy also
-<<<<<<< HEAD
     belongs_to :user
-=======
-    
->>>>>>> today_priority
 end
